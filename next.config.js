9b8f91781c4cb12c6a802b24f312416380b43598
Abdullah Belal <152031320+Abdullah-Belall@users.noreply.github.com>
2024-07-02 09:32:51 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ["image.tmdb.org", "pub-7b3c4b6d7e7a4248b4faec6a06591b68.r2.dev"],
   },
 };
