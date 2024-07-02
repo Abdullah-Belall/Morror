165ac5d46cf5d1c36561df90050a3a6ccf388fb8
Abdullah Belal <152031320+Abdullah-Belall@users.noreply.github.com>
2024-07-02 09:25:54 +0300

    MainBgImg.js

--- app/Components/MainBgImg.js
+++ app/Components/MainBgImg.js
@@ -4,7 +4,7 @@ import bgImg from "@/public/Images/bgImg.jpg";
 export default function MainBgImg() {
   return (
     <div style={{ zIndex: "-1", height: "100vh" }} className="bgImageLayer position-fixed w-100 start-0 top-0">
-      <Image className="bgImage w-100 h-100" fill style={{ objectFit: "cover" }} src={bgImg} alt="error" />
+      <Image priority className="bgImage w-100 h-100" fill style={{ objectFit: "cover" }} src={"https://pub-7b3c4b6d7e7a4248b4faec6a06591b68.r2.dev/bgImg.jpeg"} alt="error" />
     </div>
   );
 }
